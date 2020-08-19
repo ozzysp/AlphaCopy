5ad01cd77c4c14e5aea696b85acf5affdd136b11
Ozzy Scuba <48163195+ozzysp@users.noreply.github.com>
2020-08-19 07:36:18 -0300

    Delete 03-founddevices_rc.py

--- 03-founddevices_rc.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file '03-founddevices.qrc'
-#
-# Created by: PyQt5 UI code generator 5.9.2
-#
-# WARNING! All changes made in this file will be lost!
-

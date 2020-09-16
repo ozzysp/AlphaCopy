4a8038749c1699a977e05686caab2cab06ec5bb3
teddiur <ryaqueiroz@gmail.com>
2020-09-16 14:55:00 -0300

    style(copy): removed unnecessary commented code

--- src/alphacopy/copy.py
+++ src/alphacopy/copy.py
@@ -1,11 +1,5 @@
 from shutil import copy2
-# def copy_folder(folder_from: str, folder_to: str):
-# 	"""Copies an entire tree folder from folder_from to folder_to"""
 
 def copy_file(src: str, dst: str):
-	"""Copies single file from file_from to file_to"""
+	"""Copies single file from src to dst"""
 	copy2(src, dst)
-
-	# with open(file_from) as f_read:
-	# 	with open(file_to, "w") as f_write:
-	# 		f_write.write(f_read.read())

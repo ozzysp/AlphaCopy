f53e915a3c0e8377365ea9acba60572ae8ce4de4
Ozzy <ozzysp@icloud.com>
2020-10-09 22:13:04 -0300

    Function that find USB devices

--- src/alphacopy/copy.py
+++ src/alphacopy/copy.py
@@ -1,6 +1,8 @@
+# !/usr/bin/python
 from os import fsync
 from shutil import copy2, disk_usage
-
+import sys
+import usb.core
 
 def copy_file(src: str, dst: str):
     """Copies single file from src to dst"""
@@ -17,3 +19,13 @@ def check_disks(src: str, dst: str) -> bool:
     src_disk_usage = disk_usage(src)[1]
     destinaton_disk_free = disk_usage(dst)[2]
     return src_disk_usage <= destinaton_disk_free
+
+
+def scan_disks():
+    """
+    Find USB devices
+    """
+    dev = usb.core.find(find_all=True)
+    # loop through devices, printing vendor and product ids in decimal and hex
+    for cfg in dev:
+        sys.stdout.write('Hexadecimal VendorID=' + hex(cfg.idVendor) + ' & ProductID=' + hex(cfg.idProduct) + '\n\n')

c04ca4f00ef7a9b62092a3aec5c85da0eae39b95
Ozzy Scuba <48163195+ozzysp@users.noreply.github.com>
2022-02-22 01:46:54 -0300

    New functions added

--- src/alphacopy/alpha_copy.py
+++ src/alphacopy/alpha_copy.py
@@ -8,8 +8,8 @@ from datetime import datetime
 from pathlib import Path
 from humanize import naturalsize
 
-
 VOLUMES_PATH = '/System/Volumes/Data/Volumes'
+
 
 # This class defines devices functions and charateristics
 class Devices:
@@ -17,62 +17,65 @@ class Devices:
     REM_SIZE = 0
     DATE = '00/00/0000'
 
-# This function list all volumes mounted in system
-    def list_disks(self):
+    # This function list all volumes mounted in system
+    @staticmethod
+    def list_disks():
         directories = os.listdir(VOLUMES_PATH)
         return directories
 
-# This function returns used space in choosed hd
-    def used_disk(self):
+    # This function returns used space in choosed hd
+    @staticmethod
+    def used_disk():
         used_disk = psutil.disk_usage(VOLUMES_PATH)
         used_space = str(used_disk[1])
         used_bytes = str(used_space[:6])
         natural_bytes = (naturalsize(used_bytes))
         return natural_bytes
 
-# This function display free space in chosen hd
-    def free_disk(self):
+    # This function display free space in chosen hd
+    @staticmethod
+    def free_disk():
         free_disk = psutil.disk_usage(VOLUMES_PATH)
         free_space = str(free_disk[2])
         free_bytes: str = free_space[:3]
         info_free = (free_bytes + ' GB Free')
         return info_free
 
-# This function select each hd
+    # This function select each hd
     def select_from(self):
-        #found_volumes = directories
-        #return found_volumes
+        # found_volumes = directories
+        # return found_volumes
         pass
 
-# This function make new directory in target hd
-    def make_new_dir(self):
+    # This function make new directory in target hd
+    @staticmethod
+    def make_new_dir():
         now = datetime.now()
         dt_string = now.strftime("%d_%m_%Y_%H:%M")
-        path = os.getcwd()
         path = '/Users/ozz/Desktop'
         os.chdir(path)
         new_folder = dt_string
         os.mkdir(new_folder)
 
-# This function copy all content from original hd to destination hd
-    def copy_files(self, src: str, dst: str):
+    # This function copy all content from original hd to destination hd
+    @staticmethod
+    def copy_files(src: str, dst: str):
         shutil.copy2(src, dst)
         with open(dst, 'a') as f:
             os.fsync(f)
 
 # This function show progress bar bytes
-        def progress_bar_be(self):
+        def progress_bar_be():
             try:
                 while True:
                     file_copy = (sum([f.stat().st_size for f in Path('/Users/ozz/Desktop/Copied_Files').glob("**/*")]))
-                    file_origin = (sum([f.stat().st_size for f in
-                                        Path('/Volumes/Mojave HD Ext/Users/ozzy/Downloads').glob("**/*")]))
+                    file_origin = (sum([f.stat().st_size for f in Path('/Volumes/Mojave HD Ext/Users/ozzy/Downloads').glob("**/*")]))
                     if file_origin == file_copy:
                         break
             except FileNotFoundError:
                 pass
 
-# This function checks integrity of copied files
+        # This function checks integrity of copied files
         def check_hashes(self):
             directoryin = '/Volumes/CatalinaPatcher/Library'
             directoryout = '/Volumes/Mojave HD Ext/Users/ozzy/Desktop/Library'
@@ -96,9 +99,6 @@ class Devices:
                 return output
 
 
-
-
-
 def threading():
     pass
 
@@ -115,9 +115,9 @@ def eject_usb():
         os.remove(output)
         return output
 
+
 # Call class
 devices = Devices()
-
 
 # def copy_file(src: str, dst: str):
 #     """Copies single file from src to dst"""

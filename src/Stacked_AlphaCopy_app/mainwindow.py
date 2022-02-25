994dfbff3250656b8004faf0decb8ff6e6036dd8
nicmorais <nmorais.st@gmail.com>
2022-02-25 20:59:28 -0300

    Adds new DevicesUtil class

--- src/Stacked_AlphaCopy_app/mainwindow.py
+++ src/Stacked_AlphaCopy_app/mainwindow.py
@@ -2,10 +2,14 @@ from PyQt5 import uic
 from PyQt5.QtWidgets import QMainWindow
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtCore import QSize
-from alphacopy.alpha_copy import Devices
+from alphacopy.DevicesUtil import DevicesUtil
 
 
 class MainWindow(QMainWindow):
+    devices = DevicesUtil()
+    sd_label = ''
+    hdd_label = ''
+
     def __init__(self):
         super(MainWindow, self).__init__()
         uic.loadUi('Stacked_AlphaCopy_app/mainwindow.ui', self)
@@ -16,10 +20,40 @@ class MainWindow(QMainWindow):
         self.scan()
 
     def scan(self):
-        disks = Devices.list_disks()
+        disks = self.devices.list_disks()
         if len(disks) > 0:
             self.stackedWidget.setCurrentIndex(2)
+            self.set_sd_hdd(disks)
         else:
             self.stackedWidget.setCurrentIndex(1)
 
+    def set_sd_hdd(self, disks):
+        for disk in disks:
+            size = self.devices.disk_size(disk)
+            if size < 65:
+                self.sd_label = disk
+            else:
+                self.hdd_label = disk
+        self.hddLE.setText(self.hdd_label)
+        self.sdLE.setText(self.sd_label)
 
+    def copy(self):
+        self.stackedWidget.setCurrentIndex(3)
+        sd_path = self.devices.VOLUMES_PATH + '/' + self.sd_label
+        hdd_path = self.devices.VOLUMES_PATH + '/' + self.hdd_label
+        total_files = len(self.devices.list_files(sd_path))
+        self.copyProgrB.setMaximum(total_files)
+
+        def incrementProgressBar():
+            currentValue = self.copyProgrB.value()
+            self.copyProgrB.setValue(currentValue + 1)
+
+        self.devices.file_copied.connect(incrementProgressBar)
+        self.devices.copy_files(sd_path, hdd_path)
+        self.stackedWidget.setCurrentIndex(4)
+
+    def done(self):
+        self.stackedWidget.setCurrentIndex(6)
+
+    def return_to_main_page(self):
+        self.stackedWidget.setCurrentIndex(0)
